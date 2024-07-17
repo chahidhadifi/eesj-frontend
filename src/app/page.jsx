3ed57d6cd9092d25e3cde2d10b848d7772e78c72
Chahid Hadifi <chahidhadifi@gmail.com>
2024-07-17 18:21:16 +0100

    refactor: update dashboard page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,22 @@ import Link from "next/link";
 import Image from "next/image";
 import FeatherIcon from "feather-icons-react/build/FeatherIcon";
 import Sidebar from "../components/Sidebar";
-import { morning_img_02 } from "@components/imagepath";
+// import CountUp from "react-countup";
+import { morning_img_02 ,doctor_dashboard_01,
+  doctor_dashboard_02,
+  doctor_dashboard_03,
+  doctor_dashboard_04  } from "@components/imagepath";
 import { useRouter } from "next/navigation";
+
+import * as bootstrap from 'bootstrap';
 
 const Home = () => {
   const router = useRouter();
   const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  // const handleCreateDiscussion = () => {
+  //   router.push("/Formulaire");
+  // };
 
   useEffect(() => {
     const handleResize = () => {
@@ -23,12 +33,18 @@ const Home = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    window.bootstrap = bootstrap;
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  }, []) 
+
   return (
     <div id="root">
-      <Sidebar activeClassName="dashboard" />
+      <Sidebar activeClassName="/" />
       <div className="page-wrapper">
         <div className="content">
-          {/* Page Header */}
+          {/* En-tête de la page */}
           <div
             className="page-header"
             style={isSmallScreen ? { marginBottom: "70px" } : {}}
@@ -37,59 +53,116 @@ const Home = () => {
               <div className="col-sm-12">
                 <ul className="breadcrumb">
                   <li className="breadcrumb-item">
-                    <Link href="/">Page d'accueil</Link>
+                    <Link href="/">Page d'accueil </Link>
                   </li>
                   <li className="breadcrumb-item">
                     <FeatherIcon icon="chevron-right" />
                   </li>
-                  <li className="breadcrumb-item active">Page d'accueil</li>
+                  <li className="breadcrumb-item active">tableau de bord</li>
                 </ul>
               </div>
             </div>
           </div>
-          {/* Page Header */}
+          {/* En-tête de la page */}
 
           <div className="topSection">
-            {/* Bonjour Section */}
-            <div className="good-morning-blk">
+            
+
+            {/* Section Bonjour */}
+            {!isSmallScreen && ( <div className="good-morning-blk">
               <div className="row">
                 <div className="col-md-6">
                   <div className="morning-user">
                     <h2>
-                      Bonjour, <span>Dr. Khalid Amine </span>
+                      Bonjour, <span>Khalid Amine</span>
                     </h2>
-                    <p>Bonne journée au travail</p>
+                    <p>Bienvenue sur votre tableau de bord</p>
                   </div>
                 </div>
                 <div className="col-md-6 position-blk">
-                  <div className="morning-img z-index-0">
-                    <Image src={morning_img_02} alt="" />
+                  <div className="morning-img">
+                    <Image src={morning_img_02} width={190} height={190} style={{marginLeft:'91px'}} alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>)}
+            {/* Section Bonjour */}
+          </div>
+
+          {isSmallScreen && (
+            <div className="good-morning-blk">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="morning-user">
+                  <h2>
+                    Bonjour, <span>Khalid Amine</span>
+                  </h2>
+                  <p>Bienvenue sur votre tableau de bord</p>
+                </div>
+              </div>
+              <div className="col-md-6 position-blk">
+                <div className="morning-img">
+                  <Image src={morning_img_02} width={190} height={190}  alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          )}
+          <div className="doctor-list-blk">
+              <div className="row">
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget border-right-bg">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_02.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="">125</span>
+                        <span className=""></span>
+                      </h4>
+                      <h5>Patients</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget border-right-bg">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_01.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="">3/Jour</span>
+                        <span className=""></span>
+                      </h4>
+                      <h5>Consultations</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_04.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="counter-up">3/5</span>
+                        <span className="status-green" style={{cursor: 'default'}} data-bs-toggle="tooltip" data-bs-title="Si votre évaluation est faible, complétez toutes les informations de votre profil.">
+                        Remarque
+                        </span>
+                      </h4>
+                      <h5>Evaluation</h5>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            {/* Bonjour Section */}
-          </div>
 
-          {isSmallScreen && (
-            <div className="buttons-section text-center mb-4">
-              <button
-                type="button"
-                className="btn btn-primary button-creation"
-                style={{
-                  borderColor: "#2F38A3",
-                  backgroundColor: "#2F38A3",
-                  borderRadius: "20px",
-                }}
-              >
-                Créer une discussion
-              </button>
-            </div>
-          )}
-
-          
+          {/* Section Discussion */}
+         
+          {/* Section Discussion */}
         </div>
       </div>
+
     </div>
   );
 };
