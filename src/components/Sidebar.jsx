6d6692fecea76c8088e55d837155cd34ed57e5fb
Imad El Idrissi <111063251+Imad-EL-IDRISSI@users.noreply.github.com>
2024-07-21 10:20:41 +0100

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,13 +4,39 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@assets/css/font-awesome.min.css";
 import Link from "next/link";
 import Image from "next/image";
-import { dashboard, doctor, logout, menuicon10, menuicon08 } from "./imagepath";
+import { dashboard, doctor, logout, menuicon10, menuicon08, baricon, baricon1 } from "./imagepath";
 import Scrollbars from "react-custom-scrollbars-2";
 import 'boxicons';
 
 const Sidebar = (props) => {
   const [sidebar, setSidebar] = useState("");
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const toggleSubMenu = (e) => {
+    setSubMenuOpen((prev) => !prev);
+    const menuArrow = document.querySelector('#menu-item1');
+    
+      if(subMenuOpen) {menuArrow.classList.remove('subdrop') } else{ menuArrow.classList.add('subdrop');}
+      console.log(subMenuOpen)
+    
+  };
+  const handlesidebar = () => {
+    document.body.classList.toggle("mini-sidebar");
+  };
 
+  const handlesidebarmobilemenu = () => {
+    document.body.classList.toggle("slide-nav");
+    document.getElementsByTagName("html")[0].classList.toggle("menu-opened");
+    /*document
+      .getElementsByClassName("sidebar-overlay")[0]
+      .classList.toggle("opened");*/
+  };
+  const handleClick = (e, item, itemClass) => {
+    const div = document.querySelector(`#${item}`);
+    const ulDiv = document.querySelector(`.${itemClass}`);
+    ulDiv.style.display = ulDiv.style.display === 'block' ? 'none' : 'block';
+    div.classList.toggle('subdrop');
+  };
+  
   const expandMenu = () => {
     document.body.classList.remove("expand-menu");
   };
@@ -40,6 +66,17 @@ const Sidebar = (props) => {
             onMouseOver={expandMenuOpen}
           >
             <ul>
+            <Link href="#" id="toggle_btn" onClick={handlesidebar}>
+                <Image src={baricon} alt="" />
+              </Link>
+              <Link
+                href="#"
+                id="mobile_btn"
+                className="mobile_btn float-start"
+                onClick={handlesidebarmobilemenu}
+              >
+                <Image src={baricon1} alt="" />
+              </Link>
               <li className="menu-title">Menu</li>
               <li className="submenu">
                 <Link
