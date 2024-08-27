c4e8eecf0ae820139a17e630bae44bd548a9845b
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-08-27 20:20:55 +0100

    add MonProfil and Modifier profil component and services folder

--- src/app/MonProfil/page.jsx
+++ src/app/MonProfil/page.jsx
@@ -1,272 +1,212 @@
 "use client";
 
-import React from 'react'
-import { Profileuser, cameraicon, doctor, imagesend, medalicon, medalicon02, medalicon03, menuicon16, profilebg } from "../../components/imagepath";
-import Link from 'next/link';
-import "../../assets/css/style.css";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import Sidebar from '@components/Sidebar1';
-import FeatherIcon from "feather-icons-react";
-const page = () => {
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import "@assets/css/style.css";
+import Sidebar from "@components/Sidebar1";
+
+import FeatherIcon from "feather-icons-react/build/FeatherIcon";
+import { morning_img_02 ,doctor_dashboard_01,
+  doctor_dashboard_02,
+  doctor_dashboard_03,
+  doctor_dashboard_04,  
+  iconsocial02} from "@components/imagepath";
+
+import * as bootstrap from 'bootstrap';
+
+const Home = () => {
+  const router = useRouter();
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [news, setNews] = useState(null);
+
+  // const handleCreateDiscussion = () => {
+  //   router.push("/Formulaire");
+  // };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const getAllHealthNews = () => {
+    //pub_48777307746a1c7481b2890da6fa8dd3e3f8a
+    axios.get("https://newsdata.io/api/1/latest?country=ma&category=health&apikey=API_KEY")
+    .then(res => {
+      setNews(res.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  useEffect(() => {
+    window.bootstrap = bootstrap;
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    getAllHealthNews();
+  }, []) 
+
   return (
-    <div>
-      <>
-        <Sidebar id='menu-item1' id1='menu-items1' activeClassName='doctorprofile' />
-        <div className="page-wrapper">
-          <div className="content">
-            {/* Page Header */}
-            <div className="page-header">
-              <div className="row">
-                <div className="col-sm-12">
-                  <ul className="breadcrumb">
-                    <li className="breadcrumb-item"><Link href="doctors.html">Medecins </Link></li>
-                    <li className="breadcrumb-item"><i className="feather-chevron-right">
-                      <FeatherIcon icon="chevron-right" />
-                    </i>
-                    </li>
-                    <li className="breadcrumb-item active">Profil Medecin</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            {/* /Page Header */}
+    <div id="root">
+      <Sidebar activeClassName="/" />
+      <div className="page-wrapper">
+        <div className="content">
+          {/* En-tête de la page */}
+          <div
+            className="page-header"
+            style={isSmallScreen ? { marginBottom: "70px" } : {}}
+          >
             <div className="row">
               <div className="col-sm-12">
-                <div className="card">
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <div className="about-info">
-                          <h4>
-                            Profil du Docteur{" "}
+                <ul className="breadcrumb">
+                  <li className="breadcrumb-item">
+                    <Link href="/">Page d'accueil </Link>
+                  </li>
+                  <li className="breadcrumb-item">
+                    <FeatherIcon icon="chevron-right" />
+                  </li>
+                  <li className="breadcrumb-item active">tableau de bord</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* En-tête de la page */}
 
-                          </h4>
-                        </div>
-                        <div className="doctor-profile-head">
+          <div className="topSection">
+            
 
-                          <div className="row">
-                            <div>
-                              <div className="profile-user-box">
-                                <div className="profile-user-img">
-                                  <img
-                                    src={Profileuser.src}
-                                    alt="Profile"
-                                  />
-                                  <div className="form-group doctor-up-files profile-edit-icon mb-0">
-                                    <div className="uplod d-flex">
-                                      <label className="file-upload profile-upbtn mb-0">
-                                        <img
-                                          src={cameraicon.src}
-                                          alt="Profile"
-                                        />
-                                        <input type="file" />
-                                      </label>
-                                    </div>
-                                  </div>
-                                </div>
-                                <div className="names-profiles">
-                                  <h4>Dr. Bruce Willis</h4>
-                                  <h5>Médecin Senior</h5>
-                                </div>
-                                <div style={{ display: 'flex', gap: '15px', justifyContent: 'flex-end', width: '100%' }}>
-                                  <div style={{ display: 'flex', gap: '10px', justifyContent: 'flex-end' }}>
-                                    <a
-                                      className="btn"
-                                      href="https://linkedin.com"
-                                      style={{
-                                        borderColor: 'transparent',
-                                        color: 'black',
-                                        fontSize: '14px',
-                                        backgroundColor: 'transparent',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        textDecoration: 'none',
-                                        width: '30px',
-                                        width: 'auto',
-                                        padding: '0'
-                                      }}
-                                      target='_blank'
-                                    >
+            {/* Section Bonjour */}
+            {!isSmallScreen && ( <div className="good-morning-blk">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="morning-user">
+                    <h2>
+                      Bonjour, <span>Khalid Amine</span>
+                    </h2>
+                    <p>Bienvenue sur votre tableau de bord</p>
+                  </div>
+                </div>
+                <div className="col-md-6 position-blk">
+                  <div className="morning-img">
+                    <Image src={morning_img_02} width={190} height={190} style={{marginLeft:'91px'}} alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>)}
+            {/* Section Bonjour */}
+          </div>
 
-                                      <FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077B5', width: '40px' , height:'40px'}} />
-
-                                    </a>
-
-                                    <a
-                                      className="btn"
-                                      href="https://X.com"
-                                      style={{
-                                        borderColor: 'transparent',
-                                        color: 'grey',
-                                        fontSize: '14px',
-                                        backgroundColor: 'transparent',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        textDecoration: 'none',
-                                        width: '30px',
-                                        width: 'auto',
-                                        padding: '0',
-                                        marginRight: '29px'
-                                      }}
-                                      target='_blank'
-                                    >
-
-                                      <FontAwesomeIcon icon={faXTwitter} style={{
-                                        color: '#000000', fontSize: '24px', width: '38px'
-                                      }} />
-
-                                    </a>
-                                  </div>
-                                </div>
-
-                              </div>
-                            </div>
-
-
-
-                          </div>
-                        </div>
-                      </div>
+          {isSmallScreen && (
+            <div className="good-morning-blk">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="morning-user">
+                  <h2>
+                    Bonjour, <span>Khalid Amine</span>
+                  </h2>
+                  <p>Bienvenue sur votre tableau de bord</p>
+                </div>
+              </div>
+              <div className="col-md-6 position-blk">
+                <div className="morning-img">
+                  <Image src={morning_img_02} width={190} height={190}  alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          )}
+          <div className="doctor-list-blk" style={{width: '100%'}}>
+              <div className="row">
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget border-right-bg">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_02.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="">125</span>
+                        <span className=""></span>
+                      </h4>
+                      <h5>Patients</h5>
                     </div>
                   </div>
                 </div>
-                <div className="row">
-
-                  <div >
-                    <div className="doctor-personals-grp">
-                      <div className="card">
-                        <div className="card-body mt-1">
-                          <div className="personal-list-out">
-                            <div className="row">
-                              <div className="card-body ">
-                                <div className="heading-detail ">
-                                  <h4 className="mb-3">À propos de moi</h4>
-                                  <p>
-                                    Bonjour, je suis Dr. Bruce Willis, un gynécologue à l'hôpital Sanjivni de Surat.
-                                    J'aime travailler avec tout le personnel de mon hôpital et les médecins seniors.
-                                  </p>
-                                </div>
-
-                                <div className="col-xl-3 col-md-6">
-                                  <div className="detail-personal mb-4">
-                                    <h2>Nom complet</h2>
-                                    <h3>Dr.Bruce Willis</h3>
-                                  </div>
-                                  <div className="detail-personal mb-4">
-                                    <h2>Désignation</h2>
-                                    <h3>Médecin Senior</h3>
-                                  </div>
-
-
-                                  <div className="detail-personal mb-4">
-
-
-                                    <h2>Sexe</h2>
-                                    <h3>Femme</h3>
-                                  </div>
-                                  
-                                </div>
-                                <div className="col-xl-3 col-md-6 mb-4">
-                                  <div className="detail-personal">
-                                    <h2>Mobile</h2>
-                                    <h3>264-625-2583</h3>
-                                  </div>
-                                </div>
-                                <div className="col-xl-3 col-md-6 mb-4">
-                                  <div className="detail-personal">
-                                    <h2>Email</h2>
-                                    <h3>bruce@email.com</h3>
-                                  </div>
-                                </div>
-                                <div className="col-xl-3 col-md-6">
-                                  <div className="detail-personal">
-                                    <h2>Localisation</h2>
-                                    <h3>Los Angeles</h3>
-                                  </div>
-                                </div>
-                              </div>
-
-
-
-                            </div>
-                          </div>
-
-                          <div className="hello-park mb-2">
-                            <h5>Éducation</h5>
-                            <div className="table-responsive">
-                              <table className="table mb-0 border-0 custom-table profile-table">
-                                <thead>
-                                  <tr>
-                                    <th>Année</th>
-                                    <th>Diplôme</th>
-                                    <th>Institut</th>
-
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>2002-2005</td>
-                                    <td>M.D. en Médecine</td>
-                                    <td>Université du Wyoming</td>
-
-                                  </tr>
-                                  <tr>
-                                    <td>2005-2014</td>
-                                    <td>MBBS</td>
-                                    <td>Netherland Medical College</td>
-
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </div>
-                          </div>
-                          <div className="hello-park mb-2">
-                            <h5>Expérience</h5>
-                            <div className="table-responsive">
-                              <table className="table mb-0 border-0 custom-table profile-table">
-                                <thead>
-                                  <tr>
-                                    <th>Année</th>
-                                    <th>Poste</th>
-                                    <th>Hôpital</th>
-
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>2002-2005</td>
-                                    <td>Médecin Senior</td>
-                                    <td>Clinique Médicale Midtown</td>
-
-                                  </tr>
-                                  <tr>
-                                    <td>2005-2014</td>
-                                    <td>Professeur Associé</td>
-                                    <td>Netherland Medical College</td>
-
-                                  </tr>
-                                </tbody>
-                              </table>
-                            </div>
-                          </div>
-
-                        </div>
-                      </div>
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget border-right-bg">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_01.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="">3/Jour</span>
+                        <span className=""></span>
+                      </h4>
+                      <h5>Consultations</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-3 col-md-6">
+                  <div className="doctor-widget">
+                    <div className="doctor-box-icon flex-shrink-0">
+                      <img src={doctor_dashboard_04.src} alt="" />
+                    </div>
+                    <div className="doctor-content dash-count flex-grow-1">
+                      <h4>
+                        <span className="counter-up">3/5</span>
+                        <span className="status-green" style={{cursor: 'default'}} data-bs-toggle="tooltip" data-bs-title="Si votre évaluation est faible, complétez toutes les informations de votre profil.">
+                        Remarque
+                        </span>
+                      </h4>
+                      <h5>Evaluation</h5>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
 
+          {/* Section Discussion */}
+          <h3 className="text-lg mb-4 mt-5" style={{fontWeight: 'bold'}}>Dernières nouvelles sur la santé</h3>
+          <div class="row row-cols-1 row-cols-md-3 g-4">
+            {
+              news && news.map((item, i) => (
+                <div class="col" key={i}>
+                  <div class="card">
+                    <img src={item.image_url} class="card-img-top"/>
+                    <div class="card-body">
+                      <h5 class="card-title">{item.title}</h5>
+                      <h6 style={{marginTop: '-15px', marginBottom: '20px', fontSize: '10px'}}>{item.pubDate}</h6>
+                      <h6 style={{marginTop: '-10px'}}>
+                        <img src={item.source_icon} alt="" style={{marginRight: '3px', width: '20px'}} />
+                        <a href={item.source_url} style={{color: '#333448', textTransform: 'capitalize'}}>{item.source_id}</a>
+                      </h6>
+                      <p class="card-text" style={{fontSize: '11px', overflow: 'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>{item.description}</p>
+                    </div>
+                    <div class="card-body" style={{marginTop: '-20px'}}>
+                      <a href={item.link} class="btn btn-primary">Consulter la page</a>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
           </div>
-
+         
+          {/* Section Discussion */}
         </div>
+      </div>
 
-      </>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Home;
